af8c5d59f1c9786c2f0107c30102a7ca52132658
Roman Frołow <rofrol@gmail.com>
2018-10-16 16:27:43 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+## Server
+
+```bash
+$ npm i -g serve
+$ serve
+```
+
+Prepare files in `txt` folder.
+
+For example to make big file:
+
+```bash
+for i in $(seq 1 10); do cat essay.txt >> essay_10x.txt; done
+```
+
+
+```bash
+$ elm-reactor
+```
+
+Open web browser at http://localhost:8000/src/Main.elm.
+
+You may also throttle speed in Network tab in Developer Tools.
+
+https://pablohirafuji.github.io/elm-http-progress-example/
---
+++ src/Main.elm
@@ -0,0 +1,238 @@
+module Main exposing (..)
+
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (onCheck)
+import Http
+import Http.Progress as Progress exposing (Progress(..))
+
+
+main : Program Never Model Msg
+main =
+    Html.program
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+init : ( Model, Cmd a )
+init =
+    ( initModel, Cmd.none )
+
+
+
+-- Model
+
+
+type alias Model =
+    { progress : Progress String
+    , bookUrl : Maybe String
+    , bookContent : String
+    , cancel : Bool
+    }
+
+
+initModel : Model
+initModel =
+    { progress = Progress.None
+    , bookUrl = Nothing
+    , bookContent = ""
+    , cancel = False
+    }
+
+
+
+-- Subscriptions
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    if model.cancel then
+        Sub.none
+    else
+        case model.bookUrl of
+            Just bookUrl ->
+                Http.getString bookUrl
+                    |> Progress.track bookUrl GetBookProgress
+
+            Nothing ->
+                Sub.none
+
+
+
+-- Update
+
+
+type Msg
+    = NoOp
+    | GetBook String
+    | GetBookProgress (Progress String)
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
+
+        GetBook url ->
+            ( { model | bookUrl = Just url, cancel = False }, Cmd.none )
+
+        GetBookProgress (Done bookContent) ->
+            ( { model
+                | progress = Done ""
+                , bookContent = bookContent
+              }
+            , Cmd.none
+            )
+
+        GetBookProgress (Fail error) ->
+            ( { model
+                | progress = Fail error
+                , bookContent = toString error
+              }
+            , Cmd.none
+            )
+
+        GetBookProgress progress ->
+            ( { model
+                | progress = Debug.log ">>progress" progress
+                , cancel =
+                    let
+                        percent =
+                            progressLoaded progress
+                    in
+                    if percent > 50 && percent < 70 then
+                        True
+                    else
+                        False
+              }
+            , Cmd.none
+            )
+
+
+
+-- View
+
+
+view : Model -> Html Msg
+view model =
+    div [ viewStyle ]
+        [ h1 [] [ text "Elm Http.Progress example" ]
+        , p []
+            [ text "Select a book:"
+            , inputRadio "Essays - Ralph Waldo Emerson" "http://localhost:5000/txt/essays.txt"
+            , inputRadio "Essays - Ralph Waldo Emerson 10x" "http://localhost:5000/txt/essays_10x.txt"
+            , inputRadio "Leviathan - Thomas Hobbes" "http://localhost:5000/txt/leviathan.txt"
+            , inputRadio "Leviathan - Thomas Hobbes 10x" "http://localhost:5000/txt/leviathan_10x.txt"
+            , inputRadio "The Ethics of Aristotle - Aristotle" "http://localhost:5000/txt/ethics.txt"
+            ]
+        , progressView <|
+            toString <|
+                progressLoaded model.progress
+        , bookContentView model.bookContent
+        , footerView
+        ]
+
+
+inputRadio : String -> String -> Html Msg
+inputRadio labelText url =
+    div []
+        [ label
+            [ onCheck
+                (\isChecked ->
+                    if isChecked then
+                        GetBook url
+                    else
+                        NoOp
+                )
+            ]
+            [ input [ type_ "radio", name "book-radio" ] []
+            , text labelText
+            ]
+        ]
+
+
+progressView : String -> Html Msg
+progressView loaded =
+    div []
+        [ span [] [ text "Progress: " ]
+        , progress
+            [ value loaded
+            , Html.Attributes.max "100"
+            ]
+            [ text <| loaded ++ "%" ]
+        , text <| loaded ++ "%"
+        ]
+
+
+progressLoaded : Progress String -> Int
+progressLoaded progress =
+    case progress of
+        Some { bytes, bytesExpected } ->
+            round <|
+                (*) 100 <|
+                    toFloat bytes
+                        / toFloat bytesExpected
+
+        Done _ ->
+            100
+
+        _ ->
+            -- None or Fail case
+            0
+
+
+bookContentView : String -> Html Msg
+bookContentView valueText =
+    div []
+        [ textarea
+            [ value valueText
+            , disabled True
+            , bookContentViewStyle
+            ]
+            []
+        ]
+
+
+footerView : Html Msg
+footerView =
+    span []
+        [ a
+            [ href "https://gist.github.com/pablohirafuji/fa373d07c42016756d5bca28962008c4"
+            , target "_blank"
+            ]
+            [ text "Source" ]
+        , text " | "
+        , text "Books from "
+        , a
+            [ href "http://www.gutenberg.org/"
+            , target "_blank"
+            ]
+            [ text "Project Gutenberg" ]
+        ]
+
+
+
+-- Styles
+
+
+viewStyle : Attribute Msg
+viewStyle =
+    style
+        [ ( "display", "flex" )
+        , ( "flex-direction", "column" )
+        , ( "width", "550px" )
+        , ( "margin", "0 auto" )
+        , ( "font-family", "Arial" )
+        ]
+
+
+bookContentViewStyle : Attribute Msg
+bookContentViewStyle =
+    style
+        [ ( "height", "400px" )
+        , ( "width", "100%" )
+        ]
